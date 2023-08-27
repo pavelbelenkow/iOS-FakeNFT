ac2bb691d0a12b627aaae9c073f9e3846266772d
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2023-08-27 17:53:51 +0300

    Added sorting icon to navigation bar

--- FakeNFT/Presentation/CartFlow/CartViewController.swift
+++ FakeNFT/Presentation/CartFlow/CartViewController.swift
@@ -4,5 +4,30 @@ final class CartViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.NFTColor.white
+        addSubviews()
+    }
+}
+
+private extension CartViewController {
+    
+    func addSubviews() {
+        addSortingButton()
+    }
+    
+    func addSortingButton() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage.NFTIcon.sorting,
+            style: .done,
+            target: self,
+            action: #selector(sortingButtonTapped)
+        )
+        navigationItem.rightBarButtonItem?.tintColor = UIColor.NFTColor.black
+    }
+}
+
+private extension CartViewController {
+    
+    @objc func sortingButtonTapped() {
+        // TODO: add opening sorting action sheet
     }
 }
