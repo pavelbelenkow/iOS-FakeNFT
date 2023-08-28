4d3c0fb77f17b70a6c3ff20274bfd0a0bbf1e4f5
xholy6 <blast6xgit@gmail.com>
2023-08-28 18:10:13 +0300

    finished layout profile screen

---
+++ FakeNFT/Presentation/ProfileFlow/ProfileViewCell.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+final class ProfileViewCell: UITableViewCell {
+    static let reuseIdentifier = "profileCell"
+
+    private lazy var infoLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .bodyBold
+        label.textColor = UIColor.NFTColor.black
+        return label
+    }()
+
+    private lazy var arrowImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "ProfileArrow")
+        imageView.tintColor = .clear
+        return imageView
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+        activateConstraints()
+        contentView.backgroundColor = .white
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configCell(title: String, nfts count: Int) {
+        infoLabel.text = "\(title) (\(count))"
+    }
+
+    func configCell(with title: String) {
+        infoLabel.text = title
+    }
+
+    private func setupView() {
+        contentView.addSubview(infoLabel)
+        contentView.addSubview(arrowImage)
+    }
+
+    private func activateConstraints() {
+        let edge: CGFloat = 16
+        NSLayoutConstraint.activate([
+        infoLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: edge),
+        infoLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+
+        arrowImage.trailingAnchor.constraint(greaterThanOrEqualTo: contentView.trailingAnchor, constant: -edge),
+        arrowImage.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+}
--- FakeNFT/Presentation/ProfileFlow/ProfileViewController.swift
+++ FakeNFT/Presentation/ProfileFlow/ProfileViewController.swift
@@ -5,6 +5,11 @@ final class ProfileViewController: UIViewController {
     private struct ProfileConstants {
         static let editButtonSize: CGFloat = 42
         static let avatarSize: CGFloat = 70
+        static let cellHeight: CGFloat = 54
+        static let myNFTtitle = "Мои NFT"
+        static let favoritesNFTtitle = "Избранные NFT"
+        static let aboutDeveloper = "О разработчике"
+        static let cellTitles = [myNFTtitle, favoritesNFTtitle]
     }
 
     private lazy var editButton: UIButton = {
@@ -60,12 +65,26 @@ final class ProfileViewController: UIViewController {
         return label
     }()
 
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(
+            ProfileViewCell.self,
+            forCellReuseIdentifier: ProfileViewCell.reuseIdentifier
+        )
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .clear
+        return tableView
+    }()
+
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.NFTColor.white
         setupView()
         activateConstraints()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     // MARK: - Private functions
@@ -76,6 +95,7 @@ final class ProfileViewController: UIViewController {
         avatarAndNameStackView.addArrangedSubview(nameLabel)
         view.addSubview(descriptionlabel)
         view.addSubview(websiteLabel)
+        view.addSubview(tableView)
     }
 
     private func activateConstraints() {
@@ -85,7 +105,7 @@ final class ProfileViewController: UIViewController {
             editButton.widthAnchor.constraint(equalToConstant: ProfileConstants.editButtonSize),
             editButton.heightAnchor.constraint(equalToConstant: ProfileConstants.editButtonSize),
 
-            avatarAndNameStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 108),
+            avatarAndNameStackView.topAnchor.constraint(equalTo: editButton.bottomAnchor, constant: 20),
             avatarAndNameStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             avatarAndNameStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
 
@@ -97,11 +117,48 @@ final class ProfileViewController: UIViewController {
             descriptionlabel.topAnchor.constraint(equalTo: avatarAndNameStackView.bottomAnchor, constant: 20),
 
             websiteLabel.topAnchor.constraint(equalTo: descriptionlabel.bottomAnchor, constant: 8),
-            websiteLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+            websiteLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: websiteLabel.bottomAnchor, constant: 40),
+            tableView.heightAnchor.constraint(equalToConstant: ProfileConstants.cellHeight * 3)
 
         ])
 
         avatarImage.layer.cornerRadius = ProfileConstants.avatarSize / 2
         avatarImage.layer.masksToBounds = true
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension ProfileViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { 3 }
+
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: ProfileViewCell.reuseIdentifier,
+            for: indexPath
+        ) as? ProfileViewCell
+        else { return ProfileViewCell() }
+
+        switch indexPath.row {
+        case 2:
+            cell.configCell(with: ProfileConstants.aboutDeveloper)
+        default:
+            cell.configCell(title: ProfileConstants.cellTitles[indexPath.row], nfts: 12)
+        }
+        return cell
+    }
+
+}
+// MARK: - UITableViewDelegate
+extension ProfileViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        ProfileConstants.cellHeight
     }
 }
