845e958dd63bc61e3756fb2521d6aaabb25b5e88
xholy6 <blast6xgit@gmail.com>
2023-08-28 15:08:06 +0300

    added edit button

--- FakeNFT/Presentation/ProfileFlow/ProfileViewController.swift
+++ FakeNFT/Presentation/ProfileFlow/ProfileViewController.swift
@@ -1,8 +1,36 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
+
+    private lazy var editButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "ProfileEdit"), for: .normal)
+        button.tintColor = UIColor.NFTColor.black
+        return button
+    }()
+
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.NFTColor.white
+        setupView()
+        activateConstraints()
+    }
+
+    //MARK: - Private functions
+    private func setupView() {
+        view.addSubview(editButton)
+    }
+
+    private func activateConstraints() {
+        let editButtonSize: CGFloat = 42
+        NSLayoutConstraint.activate([
+            editButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -9),
+            editButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 2),
+            editButton.widthAnchor.constraint(equalToConstant: editButtonSize),
+            editButton.heightAnchor.constraint(equalToConstant: editButtonSize)
+        ])
+
     }
 }
